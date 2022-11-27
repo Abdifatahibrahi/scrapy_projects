bfa49e3a5581534d526d428e5ba5c77687a21c72
Abdifatah Ibrahim <alsidiq099@gmail.com>
2022-11-27 15:18:06 +0300

    added pagination

--- Boss/Boss/spiders/main.py
+++ Boss/Boss/spiders/main.py
@@ -5,17 +5,33 @@ class ScratchQuotes(scrapy.Spider):
     name = 'quotes'
     start_urls = ['https://quotes.toscrape.com/']
 
+    #
+    # def parse(self, response):
+    #     for div in response.css('.quote'):
+    #         quotes = div.css('.text::text').get()
+    #         yield {
+    #             'quote': quotes.replace('“', '').replace('”', ''),
+    #             'author': div.css('.author::text').get()
+    #         }
+    #
+    #     next_url = response.css('li.next a::attr(href)').get()
+    #
+    #     if next_url:
+    #         yield response.follow(next_url, callback=self.parse)
+
 
     def parse(self, response):
         for div in response.css('.quote'):
             quotes = div.css('.text::text').get()
+            author = div.css('.author::text').get()
+            tags = div.css('.tag::text').getall()
             yield {
                 'quote': quotes.replace('“', '').replace('”', ''),
-                'author': div.css('.author::text').get()
+                'author': author,
+                'tags': tags
             }
 
         next_url = response.css('li.next a::attr(href)').get()
 
         if next_url:
             yield response.follow(next_url, callback=self.parse)
-
