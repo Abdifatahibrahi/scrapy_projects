ae54f372e80216efec374cc4bee0d3993a1f5f50
Abdifatah Ibrahim <alsidiq099@gmail.com>
2022-12-15 15:54:50 +0300

    scraped products properties from different products in different pages

--- Boss/Boss/spiders/main.py
+++ Boss/Boss/spiders/main.py
@@ -12,11 +12,27 @@ class ScratchQuotes(scrapy.Spider):
         print("\n\n\n\n\n\n\n\n")
         for url in response.css(css_sel).getall():
             yield Request(url, callback=self.parseProducts)
+            break
 
     def parseProducts(self, response):
         print('\n\n\n\n\n')
         print(response.url)
         css_sel = '.product-tile-plp__gallery-wrapper a::attr(href)'
         for product_url in response.css(css_sel).getall():
-            print(product_url)
+            yield response.follow(product_url, callback=self.parseProduct)
+            break
+
+    def parseProduct(self, response):
+        print('\n\n\n\n\n')
+        print(response.url)
+        product_title = response.css('h1.pdp-stage__header-title::text').get().strip()
+        product_price = response.css('.pricing__header .pricing__main-price::text').get().strip()
+        care_inst = response.css('.care-info .care-info__text::text').getall()
+        care_inst = ' , '.join(care_inst)
+        model = response.css('#product-container-sizefit-panel::text').get().strip()
+
+        print(product_title, product_price, care_inst, model)
+
+
+
 
